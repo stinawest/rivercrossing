e95ade0798ff5cd736d919843f0f13c687af2904
stinawest <89196680+stinawest@users.noreply.github.com>
2022-01-28 10:35:32 +0100

    fjernet filer som ikke skal være i denne modulen

--- event/event.go
+++ event/event.go
@@ -3,3 +3,20 @@ package event
 func Put(item string) string {
     return "[kylling rev korn ---\\ \\_korn_/ _________________/---]"
 }
+
+
+func ViewState() string {
+	// Sjekke data som er lagret ("kylling til venstre", "rev til venstre")
+	return "[kylling rev korn hs ---\\ \\__/ _________________/---]"
+}
+
+
+
+
+func CrossRiver() 
+return //returnerer ny tilstand
+
+func Reset()
+
+
+func Println()
--- event/event_test.go
+++ event/event_test.go
@@ -3,13 +3,33 @@
 // start() or setup(), reset() osv.
 package event
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
+
 
 func TestPut(t *testing.T) {
     // Hva forventer jeg?
     wanted := "[kylling rev korn ---\\ \\_korn_/ _________________/---]"
     got := Put("korn") // Hva fikk jeg?
     if got != wanted {
-             t.Errorf("Feil, fikk %q, ønsket %q.", got, wanted)
+             t.Errorf("Feil, fikk %q, ønsket %q.", got, wanted) 
     }
+}
+
+
+
+
+func PutInBoat(t *testing.T) {
+	wanted := "[kylling rev korn ---\\ \\_kylling_/ _________________/---]"
+	got := PutInBoat("kylling") 
+	if got != wanted {
+			t.Errorf("Feil, fikk %q, ønsket %q", got, wanted)
+	}
+}
+
+
+
+
 }
